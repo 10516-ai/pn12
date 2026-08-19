6d563954123aea6f0df9ded94d40076b490e5de6
10516-ai <10516@khamphoempittaya.ac.th>
2026-08-19 13:45:48 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'science-quiz-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+  // หากมีไฟล์ css หรือ js แยกต่างหาก ให้เพิ่มพาธไว้ที่นี่ด้วย เช่น './style.css', './script.js'
+];
+
+// ติดตั้ง Cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// เรียกใช้งาน Cache เมื่อออฟไลน์
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
