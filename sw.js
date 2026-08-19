f28e7aa9d44e79d8e79dbab0da61ea8fd92f527f
10516-ai <10516@khamphoempittaya.ac.th>
2026-08-19 13:49:22 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './android-chrome-192x192.png',
+  './android-chrome-512x512.png'
   // หากมีไฟล์ css หรือ js แยกต่างหาก ให้เพิ่มพาธไว้ที่นี่ด้วย เช่น './style.css', './script.js'
 ];
 
